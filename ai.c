ed44689a58efa6489006b8ac16b282e299df5f06
tylerszeto <tylerszeto@gmail.com>
2018-05-12 17:20:29 -0700

    a hard coded list of commands to avoid the box. Shows how AI would work once some path finding is implemented

--- ai.c
+++ ai.c
@@ -3,6 +3,7 @@
 void getDecision(struct cube* redCube, int tock){
 
     if(tock ==0){
+    redCube->horizontalVel = 0;
     redCube->moveRight = 1; //toggle moving right
     }
 
@@ -12,8 +13,9 @@ void getDecision(struct cube* redCube, int tock){
 
 
     if(tock==150){
-    redCube->moveUp = 1;
-    }
+    redCube->verticalVel  = 0; //AI needs to mimick the player inputs, look at how keyboard movements are handled. Similiar commands
+    redCube->moveUp = 1;        //need to be called in order for the movement to look natural, since the "engine" was originally
+    }                           //meant to be human controlled
 
 
     if(tock==240){
@@ -21,18 +23,20 @@ void getDecision(struct cube* redCube, int tock){
     }
 
     if(tock==320){
+    redCube->horizontalVel = 0;
     redCube->moveRight = 1;
     }
 
-    if(tock==400){
+    if(tock==440){
     redCube->slowDownHorizontal = 1;
     }
 
-    if(tock == 465){
+    if(tock == 570){
+    redCube->verticalVel = 0;
     redCube->moveDown = 1;
     }
 
-    if(tock ==540){
+    if(tock ==700){
     redCube->slowDownVertical = 1;
     }
 
